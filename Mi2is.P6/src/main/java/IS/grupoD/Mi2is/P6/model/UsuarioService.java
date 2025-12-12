c37be588ee03820a84f00640a2e372b64040a439
Berica99 <belenrios@uma.es>
2025-12-12 11:19:33 +0100

    UsuarioService modificado

--- Mi2is.P6/src/main/java/IS/grupoD/Mi2is/P6/model/UsuarioService.java
+++ Mi2is.P6/src/main/java/IS/grupoD/Mi2is/P6/model/UsuarioService.java
@@ -1,23 +1,25 @@
 package IS.grupoD.Mi2is.P6.model;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import java.util.List;
 
 @Service
 public class UsuarioService {
-    @Autowired
-    private RepositoryUsuario repositoryUsuario;
 
-    public java.util.List<Usuario> getAllUsuarios() {
+    private final RepositoryUsuario repositoryUsuario;
+
+    public UsuarioService(RepositoryUsuario repositoryUsuario) {
+        this.repositoryUsuario = repositoryUsuario;
+    }
+
+    public List<Usuario> getAllUsuarios() {
         return repositoryUsuario.findAll();
     }
-    
 
     public Usuario addUsuario(Usuario u) {
         if (u == null) {
             throw new IllegalArgumentException("Usuario cannot be null");
         }
-        return repositoryUsuario.saveAndFlush(u);
+        return repositoryUsuario.save(u);
     }
 }
