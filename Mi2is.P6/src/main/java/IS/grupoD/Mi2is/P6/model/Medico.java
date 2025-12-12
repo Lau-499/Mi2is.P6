140af1f8ee9e54b342ccf64bbbab66d8fd8accae
Berica99 <belenrios@uma.es>
2025-12-12 11:02:37 +0100

    Clase medico creada

---
+++ Mi2is.P6/src/main/java/IS/grupoD/Mi2is/P6/model/Medico.java
@@ -0,0 +1,21 @@
+package IS.grupoD.Mi2is.P6.model;
+
+public class Medico {
+        
+    private String especialidad;
+    public String getEspecialidad() {
+        return especialidad;
+    }
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    private String CentroMedico;
+    public String getCentroMedico() {
+        return CentroMedico;
+    }      
+    public void setCentroMedico(String centroMedico) {
+        this.CentroMedico = centroMedico;
+    }
+
+}
