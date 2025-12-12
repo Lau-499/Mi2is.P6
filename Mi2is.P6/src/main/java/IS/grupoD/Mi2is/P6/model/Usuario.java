2e52050a973851b57ac1440f9f74add1a4b0c445
Lau-499 <laurasanchezw@gmail.com>
2025-12-12 11:41:03 +0100

    c

---
+++ Mi2is.P6/src/main/java/IS/grupoD/Mi2is/P6/model/Usuario.java
@@ -0,0 +1,49 @@
+package IS.grupoD.Mi2is.P6.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    private String contrasenya;
+    public String getContrasenya() {
+        return contrasenya;
+    }
+    public void setContrasenya(String contrasenya) {
+        this.contrasenya = contrasenya;
+    }
+
+    private String nombre;
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    private String apellidos;
+    public String getApellidos() {
+        return apellidos;
+    }
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+
+    //hola caracola (la he borrado, pero dejo el comentario -Laura)
+    
+    
+}
