54e016f65e05ab041846d36ee29c5a95cf14c385
Lau-499 <laurasanchezw@gmail.com>
2025-12-12 11:10:24 +0100

    Clase paciente creada

---
+++ Mi2is.P6/src/main/java/IS/grupoD/Mi2is/P6/model/Paciente.java
@@ -0,0 +1,19 @@
+package IS.grupoD.Mi2is.P6.model;
+
+import java.time.LocalDate;
+
+public class Paciente {
+    private String nss;
+
+    private String dni;
+
+    private String email;
+
+    private String telefono;
+
+    private LocalDate fechaNacimiento;
+
+
+
+
+}
